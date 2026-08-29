cae9a8a86665b2e489413420a04e2f7b1c7dcf7f
mayank <belchandanmayank02@gmail.com>
2026-08-29 13:52:15 +0530

    created the class structure and implemented basic frame and a top bar on the app

---
+++ src/gui/WhiteboardGUI.java
@@ -0,0 +1,52 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class WhiteboardGUI extends JFrame {
+
+
+    public WhiteboardGUI() {
+
+        setTitle("ZeroBoard");
+        setSize(1200, 750);
+        setMinimumSize(new Dimension(900, 600));
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+
+        // MAIN CONTAINER
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBackground(new Color(30, 30, 30));
+        add(mainPanel);
+
+        //TOP BAR OF THE BOARD
+        JPanel topBar = createTopBar();
+        mainPanel.add(topBar, BorderLayout.NORTH);
+    }
+        private JPanel createTopBar() {
+
+            JPanel panel = new JPanel(new BorderLayout());
+
+            panel.setPreferredSize(new Dimension(0, 55));
+            panel.setBackground(new Color(25, 25, 25));
+
+            JLabel title = new JLabel("  ZeroBoard");
+
+            title.setForeground(Color.WHITE);
+            title.setFont(new Font("SansSerif", Font.BOLD, 22));
+
+            JLabel status = new JLabel("LAN COLLABORATIVE WHITEBOARD  ");
+
+            status.setForeground(new Color(150, 150, 150));
+            status.setFont(new Font("SansSerif", Font.PLAIN, 12));
+
+            panel.add(title, BorderLayout.WEST);
+            panel.add(status, BorderLayout.EAST);
+
+            return panel;
+        }
+    }
+
